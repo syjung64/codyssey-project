496f5e527e2f85a956c5054675ff3efe74287b1f
syjung64 <syjung@dongyang.ac.kr>
2025-02-20 15:46:30 +0900

    단계1-2

---
+++ main_1_2.py
@@ -0,0 +1,41 @@
+import json
+
+def read_log_file(filename) :
+    log_list = []
+    
+    with open(filename, "r", encoding='utf-8') as file:
+        lines = file.readlines()
+
+        # 1st line skip
+        for line in lines[1:]:
+            parts = line.strip().split(',', 2)
+            if len(parts) == 3 :
+                log_list.append(parts)
+    return log_list
+
+def lst_to_dict(log_entry) :
+    log_dict = {}
+    for entry in log_entry :
+        log_dict[entry[0]] = {"event": entry[1], "message": entry[2]}
+    return log_dict
+
+def save_to_json(log_dict, filename) :
+
+    with open(filename, "w", encoding="utf-8") as json_file:
+        json.dump(log_dict, json_file, indent=4)
+
+log_filename = "mission_computer_main.log"
+json_filename = "mission_computer_main.json"
+
+log_entry = read_log_file(log_filename)
+
+log_entry.sort(reverse=True)
+
+# list를 dictionary로 변경
+log_dict = lst_to_dict(log_entry)
+
+# JSON 파일로 저장
+save_to_json(log_dict, json_filename)
+
+for entry in log_entry:
+    print(entry)
