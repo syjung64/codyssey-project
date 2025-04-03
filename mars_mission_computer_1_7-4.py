ce96e184b9ac1bd5731b5819aaa6d3f019de95d7
syjung64 <syjung@dongyang.ac.kr>
2025-04-03 11:10:37 +0900

    03문제7

---
+++ mars_mission_computer_1_7-4.py
@@ -0,0 +1,96 @@
+import random
+import time
+import threading
+
+log_file = 'env.log'
+
+# 환경 변수 범위 설정 (class 변수)
+env_values = {
+    "mars_base_internal_temperature": [(18, 30.5), 18, 0, '도'],
+    "mars_base_external_temperature": [(0, 21), 0, 0, '도'],
+    "mars_base_internal_humidity": [(50, 60), 50, 0, '%'],
+    "mars_base_external_illuminance": [(500, 715), 500, 0, 'W/m2'],
+    "mars_base_internal_co2": [(0.02, 0.1), 0.02, 0, '%'],
+    "mars_base_internal_oxygen": [(4, 7), 4, 0, '%']
+}
+
+class MissionComputer:
+
+    def __init__(self, env_values):
+        self.env_values = env_values
+        self.stop_flag = False
+
+    def key_listener(self):
+        while True:
+            key = input()
+            if key == 'q':
+                print('졸료 키 q 눌림')
+                self.stop_flag = True
+                break
+        
+    def get_sensor_data(self, ds, interval=10, ave_interval_min = 1) :
+        listener_thread = threading.Thread(target=self.key_listener, daemon=True)
+        listener_thread.start()
+
+        ave_values_sec = ave_interval_min * 60
+        loop = ave_values_sec // interval
+        while not self.stop_flag:
+            print("\n환경 변수 업데이트:")
+            ds.set_env()
+            self.env_values = ds.get_env()
+
+            json_str = '{\n'
+            for key in self.env_values.keys():
+                json_str += f'\t"{key}": {self.env_values[key][1]:.2f},\n'
+            json_str = json_str.rstrip(',\n') + '\n}'
+            print(json_str)
+                        
+            if ave_values_sec > 0 :
+                ave_values_sec = ave_values_sec - interval
+
+                for key in self.env_values.keys():
+                    self.env_values[key][2] += self.env_values[key][1]
+            else :
+                for key in self.env_values.keys():
+                    print(f"{ave_interval_min} 분 평균 - {key}: {(self.env_values[key][2]/loop):.2f})")
+                    self.env_values[key][2] = 0
+                ave_values_sec = ave_interval_min * 60
+
+            time.sleep(interval)
+
+class DummySensor:
+    def __init__(self, env_values):
+        self.env_values = env_values
+
+        if not self.__file_exist(log_file):
+            with open(log_file, 'a') as log:
+               headers = self.env_values.keys()
+               log.write('Date | ' + ' | '.join(headers) + '\n')
+
+    def __file_exist(self, log_file):
+        try:
+            with open(log_file, 'r'):
+                return True
+        except FileNotFoundError:
+            return False   
+        
+    def get_env(self):
+        return self.env_values
+
+    def set_env(self):
+        now_str = time.strftime("%Y-%m-%d %H:%M:%S", time.localtime())
+        for key in self.env_values :
+            self.env_values[key][1] = random.uniform(*self.env_values[key][0])
+
+        with open(log_file, 'a') as log :
+            values = list(map(lambda x: str(f'{x[1]:.2f}'), self.env_values.values()))
+            log.write(f'{now_str} | ' + ' | '.join(values) + '\n')
+
+# DummySensor 객체 생성
+ds = DummySensor(env_values)
+
+# MissionComputer 객체 생성
+RunComputer = MissionComputer(env_values)
+
+#ds.set_env()
+RunComputer.get_sensor_data(ds, interval=20)
